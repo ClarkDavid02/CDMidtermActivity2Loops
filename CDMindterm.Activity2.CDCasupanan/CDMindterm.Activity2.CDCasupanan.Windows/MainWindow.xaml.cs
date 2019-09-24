68d1b488e710e7402e591f24c7cb13c223a2cf7d
Kalvenven <41784087+Kalvenven@users.noreply.github.com>
2019-09-24 16:38:31 +0800

    CDMidtermActivity2Loops

---
+++ CDMindterm.Activity2.CDCasupanan/CDMindterm.Activity2.CDCasupanan.Windows/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CDMindterm.Activity2.CDCasupanan.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnButton_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void BtnButton_Clic(object sender, RoutedEventArgs e)
+        {
+            int From = int.Parse(TextBox_From.Text);
+            int To = int.Parse(TextBox_To.Text);
+
+            int sum = 0;
+            for (int x = From; x <= To; x++)
+            {
+                sum += x;
+            }
+            lblResult.Content = sum.ToString();
+        }
+    }
+}
